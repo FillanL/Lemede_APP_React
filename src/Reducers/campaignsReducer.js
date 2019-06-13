d90f31c732cadacebfd2069f97e0cdcce8f785f6
Locallize <Fillanlee@aol.com>
2019-06-12 20:21:47 -0400

    new campaigns auto render

--- src/Reducers/campaignsReducer.js
+++ src/Reducers/campaignsReducer.js
@@ -23,7 +23,7 @@ export default function (state = initialState, action) {
     case NEW_CAMPAIGN:
       return {
         ...state,
-        newCampaign: action.payload
+        campaigns: [...state.campaigns, action.payload]
       };
     case SELECT_CAMPAIGN:
       return {
